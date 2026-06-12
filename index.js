a3ab7b11dc6c9ecf4f2383c7cac2d31870c7ec37
Richard Wasonga <rowwasonga@gmail.com>
2026-06-12 16:55:43 +0300

    refresh cycle #24 variables

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-console.log('Hello');
-console.log("I like pizza");
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
 
-window.alert('This is an alert')
-window.alert(`I like pizza`)
+// let firstName = "Row"
+// let favoriteFood = "Pizza";
+// let email = "row123@gmail.com"
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza`;
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+let fullName = "Ricahrd Wasonga"
+let age = 20;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled ${isStudent}`;
