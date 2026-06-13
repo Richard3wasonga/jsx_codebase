af8e70a35ef374d6af0f591c1587bc260c1cdd52
Richard Wasonga <rowwasonga@gmail.com>
2026-06-13 19:16:58 +0300

    refresh cycle #27 type conversion

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// let username = window.prompt("What's your username?");
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1;
 
-// console.log(username);
+// console.log(age, typeof age);
 
-let username;
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`;
-}
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
