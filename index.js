861bee9a5cf4bfa1611b362cc7087d94599e4c2c
Richard Wasonga <rowwasonga@gmail.com>
2026-06-17 19:51:59 +0300

    refresh cycle #33 checked property

--- index.js
+++ index.js
@@ -1,70 +1,30 @@
-// let age = 13;
-// if(age >= 18){
-//     console.log("You are old enough to eneter this site");
-// }
-// else{
-//     console.log("You must be 18+ to enter");
-// }
-
-
-// let time = 14;
-
-// if(time < 12){
-//     console.log("Good morning")
-// }
-// else{
-//     console;.log("Good afternoon")
-// }
-
-
-// let isStudent = false;
-
-// if(isStudent){
-//     console.log("You are a student");
-// }
-// else{
-//     console.log("You are not a student");
-// }
-
-// let age = 18;
-// let hasLicense = false;
-
-// if(age >= 16){
-//     console.log("You are old enough to drive")
-
-//     if(hasLicense){
-//         console.log("You have your license!");
-//     }else{
-//         console.log("You do not have your license yet!");
-//     }
-// }
-// else{
-//     console.log("You must be 16+ to have a license")
-// }
-
-const myText = document.getElementById("myText");
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
 const mySubmit = document.getElementById("mySubmit");
-const resultElement = document.getElementById("resultElement");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
 
-let age;
-
-mySubmit.onclick = function (){
-    age = myText.value 
-    age = Number(age)
-    if(age >= 100){
-        resultElement.textContent = `You are too old to enter this site`;
-    }
-    else if(age == 0){
-        resultElement.textContent = `You can't enter. you were just born`;
-    }
-    else if(age >= 18){
-        resultElement.textContent = `You are old enough to enter this site`;
-    }
-    else if(age < 0){
-        resultElement.textContent = `Your age can't be below 0`;
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed!`;
     }
     else{
-        resultElement.textContent = `You must be 18+ to enter this site`;
+        subResult.textContent = `You are not subscribed!`;
+    }
+
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with MasterCard`;
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+    else{
+        paymentResult.textContent = `You must select a payment type`;
     }
 }
-
