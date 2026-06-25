f76469b1dd90f53f8be4f3ee477c74268f9aac73
Richard Wasonga <rowwasonga@gmail.com>
2026-06-25 20:26:56 +0300

    refresh cycle #41 while and do-while loop

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const PI = 3.14;
+// let username = "";
 
-// if(PI === "3.14"){
-//     console.log("That is PI");
+// while(username === "" || username === null){
+//     username = window.prompt(`Enter your name`);
 // }
-// else{
-//     console.log("That is not PI")
-// }
+// console.log(`Hello ${username}`);
 
 
-if(PI !== "3.14"){
-    console.log("That is PI");
+// let username;
+
+// do{
+//     username = window.prompt(`Enter your name`);
+// }while(username === "" || username === null)
+// console.log(`Hello ${username}`);
+
+let loggedIn = false;
+let username;
+let password;
+
+while(!loggedIn){
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in!");
+    }
+    else{
+        console.log("Invaid credentials! please try again");
+    }
 }
-else{
-    console.log("That is not PI")
-}
+
+// do{
+//     username = window.prompt(`Enter your username`);
+//     password = window.prompt(`Enter your password`);
+
+//     if(username === "myUsername" && password === "myPassword"){
+//         loggedIn = true;
+//         console.log("You are logged in!");
+//     }
+//     else{
+//         console.log("Invaid credentials! please try again");
+//     }
+// }while(!loggedIn)
