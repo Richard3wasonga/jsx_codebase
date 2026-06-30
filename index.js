55bfced1a868de330a46c4bfb0495550d1924d1e
Richard Wasonga <rowwasonga@gmail.com>
2026-06-30 19:34:27 +0300

    refresh cycle #43 number guiessing game

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-// for(let i = 10; i > 0; i-=3){
-//     console.log(i);
-// }
-// console.log("HAPPY NEW YEAR!");
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-for(let i = 1; i <= 20; i++){
-    if(i == 13){
-        // continue;
-        break;
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
     }
     else{
-        console.log(i);
+        attempts++;
+        if(guess < answer){
+            window.alert("TOO LOW! TRY AGAIN!");
+        }
+        else if(guess > answer){
+            window.alert("TOO HIGH! TRY AGAIN!");
+        }
+        else{
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
+            running = false;
+        }
     }
-    }
-    
+}
